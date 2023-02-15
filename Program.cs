0e687a4ef231f0d06eea7427f1e1f3078f6f8723
kathleenelay <kelay@crimson.ua.edu>
2023-02-15 13:13:30 -0600

    lab 4

--- Program.cs
+++ Program.cs
@@ -1,2 +1,79 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string choice = "";
+bool getMenu = true;
+while (getMenu != false)
+{
+    System.Console.WriteLine("View Full Triangle - Enter 1 / View Partial Triangle - Enter 2 / Enter 3 - Exit Menu");
+    choice = Console.ReadLine();
+    switch(choice)
+    {
+        case "1":
+            GetFull();
+            getMenu = true;
+            break;
+        case "2":
+            GetPartial();
+            getMenu = true;
+            break;
+
+        case "3":
+            getMenu = false;
+            break;
+
+        default: 
+            Invalid();
+            getMenu = true;
+            break;
+    }
+}
+
+static void GetFull()
+{
+    Console.Clear();
+    Random row = new Random();
+    int rowAmt = row.Next(3, 10);
+    for(int j=0; j < rowAmt; j++)
+    {
+        for(int i=0; i<=j; i++)
+        {
+            Console.Write("0");
+        }
+    }
+    ReturnMenu();
+}
+
+static void GetPartial()
+{
+    Console.Clear();
+    Random row = new Random();
+    int rowAmt = row.Next(3,10);
+    for(int j=0; j<rowAmt; j++)
+    {
+        for(int i=0; i<=j; i++)
+        {
+            Random missing = new Random();
+            int spot = missing.Next(2);
+            if(spot==1)
+            {
+                System.Console.Write("0");
+            }
+            else Console.Write(" ");
+        }
+        System.Console.WriteLine();
+    }
+    ReturnMenu();
+}
+
+static void Invalid()
+{
+    System.Console.WriteLine("Invalid entry");
+    ReturnMenu();
+}
+
+static void ReturnMenu()
+{
+    Console.Clear();
+    System.Console.WriteLine("Press any key to return to menu");
+    Console.ReadKey();
+
+}
+
